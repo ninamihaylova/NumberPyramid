aaad672cd615de774766249e0d70f99d0645c073
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-27 15:59:09 +0300

    Initial commit

---
+++ NumberPyramid/Program.cs
@@ -0,0 +1,26 @@
+﻿class NumberPyramid
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int currentNumber = 1;
+
+        for (int i = 1; i < n; i++)
+        {
+
+            for (int row = 1; currentNumber <= n; row++)
+            {
+
+                for (int col = 1; col <= row && currentNumber <= n; col++)
+                {
+                    Console.Write($"{currentNumber} ");
+                    currentNumber++;
+                }
+
+                Console.WriteLine();
+
+            }
+
+        }
+    }
+}
